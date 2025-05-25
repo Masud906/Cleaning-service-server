e2521724926e9138367df2504cb39b813b9a9a03
masud rana <Masud Rana>
2025-05-25 12:53:37 +0600

    last commit

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config()
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dejaj4w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -32,143 +31,140 @@ async function run() {
     const reviewsCollection = client.db("CleningBD").collection("reviews");
     const cartCollection = client.db("CleningBD").collection("carts");
     //jwt related api
-    app.post('/jwt', async (req, res) => {
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '1h'
+        expiresIn: "1h",
       });
-      console.log(token)
+      console.log(token);
       res.send({ token });
-      
-    })
+    });
 
     //middlewares
     const verifyToken = (req, res, next) => {
-      console.log('inside verify token', req.headers.authorization);
+      console.log("inside verify token", req.headers.authorization);
       if (!req.headers.authorization) {
-        return res.status(401).send({ message: 'unauthorize access' });
+        return res.status(401).send({ message: "unauthorize access" });
       }
-      const token = req.headers.authorization.split(' ')[1];
+      const token = req.headers.authorization.split(" ")[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
-          return res.status(401).send({ message: 'unauthorize access' })
+          return res.status(401).send({ message: "unauthorize access" });
         }
         req.decoded = decoded;
         next();
-      })
-    }
+      });
+    };
     // const verifyToken = (req, res, next) => {
     //   console.log('inside verify token', req.headers.authorization);
     //   res.send(result);
     // }
 
     //users related api verifyToken,
-    app.get('/users',verifyToken, async (req, res) => {
+    app.get("/users", verifyToken, async (req, res) => {
       console.log(req.headers);
       const result = await userCollection.find().toArray();
       res.send(result);
     });
 
-    app.post('/users', async (req, res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
       //inserted email if user dosent exist
       //you can do this many ways
-      const query = { email: user.email }
-      const existingUser = await userCollection.findOne(query)
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
       if (existingUser) {
-        return res.send({ message: 'user alrady exists', insertedId: null })
+        return res.send({ message: "user alrady exists", insertedId: null });
       }
       const result = await userCollection.insertOne(user);
       res.send(result);
-    })
+    });
     //admin role
-    app.patch('/users/admin/:id', async (req, res) => {
+    app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
-          role: 'admin'
-        }
-      }
-      const result = await userCollection.updateOne(filter, updatedDoc)
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
-    })
+    });
     //users related api//
-    app.delete('/users/:id', async (req, res) => {
+    app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
+      const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
       res.send(result);
-    })
+    });
     //admin chake
-    app.get('/users/admin/:email', verifyToken, async(req, res) =>{
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
-  
-      if(email !== req.decoded.email) {
-        return res.status(403).send({message: 'forbidden access'})
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
       }
-  
-      const query = {email: email};
+
+      const query = { email: email };
       const user = await userCollection.findOne(query);
       let admin = false;
-      if(user){
-        admin = user?.role === 'admin';
+      if (user) {
+        admin = user?.role === "admin";
       }
-      res.send({admin});
-     })
+      res.send({ admin });
+    });
 
-      // //use verify admin after verifyToken
+    // //use verify admin after verifyToken
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const query = {email: email};
+      const query = { email: email };
       const user = await userCollection.findOne(query);
-      const isAdmin = user?.role === 'admin';
-      if(!isAdmin) {
-        return res.status(403).send({message: 'forbidden assess' });
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden assess" });
       }
       next();
-    }
-
+    };
 
     //service reladed api
-    app.get('/service', async (req, res) => {
+    app.get("/service", async (req, res) => {
       const result = await serviceCollection.find().toArray();
       res.send(result);
-    })
+    });
     //review reladed api
-    app.get('/reviews', async (req, res) => {
+    app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
-    })
+    });
     //service delet //verifyToken,verifyAdmin,
-    app.delete('/service/:id', async (req, res) => {
+    app.delete("/service/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: (id) }
+      const query = { _id: id };
       const result = await serviceCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     // cars collection
-    app.get('/carts', async (req, res) => {
+    app.get("/carts", async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
-    app.post('/carts', async (req, res) => {
+    app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
       res.send(result);
-    })
+    });
     //delet
-    app.delete('/carts/:id', async (req, res) => {
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
-
-    })
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 }); // deploy comment
@@ -180,12 +176,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('boss is running')
-})
+app.get("/", (req, res) => {
+  res.send("boss is running");
+});
 
 app.listen(port, () => {
-  console.log(`boss resturent on port: ${port}`)
-})
+  console.log(`boss resturent on port: ${port}`);
+});
